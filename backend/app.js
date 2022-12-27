1a0086e311165dd23409115d62f9e4d73639647f
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2022-12-27 19:07:53 +0900

    remove unnecessary code

--- backend/app.js
+++ backend/app.js
@@ -16,14 +16,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Groupomania!" })
 });
 
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   next();
-// });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
