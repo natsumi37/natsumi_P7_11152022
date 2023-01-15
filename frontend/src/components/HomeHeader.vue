9c8c8d74e61f48dfc7a5faf340fe7a3a79cf019b
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-16 06:05:08 +0900

    add user logout and delete functions

--- frontend/src/components/HomeHeader.vue
+++ frontend/src/components/HomeHeader.vue
@@ -1,28 +1,64 @@
 <template>
   <header class="header-box">
     <img class="header-logo" src="@/assets/logo/icon-left-font-monochrome-white.png" alt="Groupomania logo">
-    <img class="header-profile-pic" :src="users[0].profilePic" :alt="'profile picture of ' + userFullName">
+    <div class="header-profile-pic" :src="loggedinUser.profilePicUrl" :alt="'profile picture of ' + userFullName"></div>
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <font-awesome-icon icon="fa-solid fa-right-from-bracket" color="white" style="font-size: 25px" />
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><button class="dropdown-item" @click="logout">Logout</button></li>
+            <li><button class="dropdown-item" @click="deleteAccount">Delete Account</button></li>
+          </ul>
+        </li>
+      </ul>
   </header>
 </template>
 
 <script>
-import { mapState } from "vuex"
+import { mapState, mapGetters } from "vuex"
 
 export default {
   data: function() {
     return {
-
     }
   },
   computed: {
-    ...mapState({
-      users: state => state.users,
-      userFullName: state => state.users.firstName + " " + state.users.lastName
-    })
+    ...mapState([
+      "auth", "loggedinUser"
+    ]),
+    ...mapGetters({
+      userFullName: "getUserFullName"
+    }),
+  },
+  methods: {
+    logout() {
+      return this.$store.dispatch("logout").then(
+        () => {
+          console.log("User Logged out!")
+          this.$router.push("/login")
+        }
+      ).catch(
+        (error) => {
+          throw error
+        }
+      )
+    },
+    deleteAccount() {
+      return this.$store.dispatch("deleteAccount").then(
+        () => {
+          console.log("User deleted!")
+          this.$router.push("/")
+        }
+      ).catch(
+        (error) => {
+          throw error
+        }
+      )
+    }
   }
 }
-
-
 
 </script>
 
@@ -32,16 +68,20 @@ export default {
     background-color: orangered;
     height: 70px;
     display: flex;
-    justify-content: space-between;
     align-items: center;
     padding: 0 20px;
+    @media screen and (max-width: 320px) {
+      padding: 0 10px;
+    }
   }
   &-logo {
     width: 200px;
     height: 70px;
+    margin-right: auto;
     object-fit: cover;
     @media screen and (max-width: 320px) {
-      width: 70%;
+      width: 65%;
+      margin-right: 5px;
     }
   }
   &-profile-pic {
@@ -49,7 +89,10 @@ export default {
     height: 50px;
     border: white solid 1px;
     border-radius: 50%;
-    object-fit: cover;
+    margin-left: auto;
+    // object-fit: cover;
+    z-index: 5;
+    margin-right: 10px;
   }
 }
 
