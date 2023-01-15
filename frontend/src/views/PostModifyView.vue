5233834e44343d9448663036494a74adf9362e9e
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-16 06:23:25 +0900

    add modify view

---
+++ frontend/src/views/PostModifyView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <HomeHeader />
+    <div class="container-box">
+      <HomeSidebar />
+      <PostModify />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex"
+
+export default {
+  name: "PostModifyView",
+  components: {
+    "HomeHeader": require("@/components/HomeHeader.vue").default,
+    "HomeSidebar": require("@/components/HomeSidebar.vue").default,
+    "PostModify": require("@/components/PostModify.vue").default
+  },
+}
+
+</script>
+
+<style lang="scss">
+.container-box {
+  display: flex;
+}
+
+@media screen and (max-width: 760px) {
+  .container-box {
+    flex-direction: column-reverse;
+  }
+}
+</style>
