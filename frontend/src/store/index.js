618a769652c551663d37a51dde89db2ffddd5173
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-22 17:32:19 +0900

    rewrite code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,19 +5,7 @@ import PostDataService from "../services/PostDataService";
 
 export default createStore({
   state: {
-    /* 
-     store state: {
-      singleUserForm: {} - signup, login
-      SinglePostForm: {} - createSinglePost, modifySinglePost,
-      allUsers: []
-      allPosts:[]
-      singlePost: {} - getSinglePost, deleteSinglePost, readPost, likePost
-      auth: {}
-      loggedinUser: {}
-    }
-    */
     singleUserForm: {
-      userId: "",
       firstName: "",
       lastName: "",
       email: "",
@@ -30,21 +18,7 @@ export default createStore({
       content: "",
       contentImgUrl: "",
       userId: "",
-      readPostId: [],
-      likePostId: []
     },
-    allUsers: [],
-    allPosts: [],
-    singlePost: {
-      postId: "",
-      title: "",
-      content: "",
-      contentImgUrl: "",
-      userId: "",
-      readPostId: [],
-      likePostId: []
-    },
-    unreadPosts: [],
     auth: {
       userId: "",
       token: ""
@@ -55,12 +29,26 @@ export default createStore({
       email: "",
       profilePicUrl: ""
     },
+    allPosts: [],
+    singlePost: {
+      postId: "",
+      title: "",
+      content: "",
+      contentImgUrl: "",
+      userId: "",
+      ReadPosts: [],
+      LikePosts: []
+    },
+    unreadPosts: [],
+    allUsers: [],
   },
+
   getters: {
     getUserFullName(state) {
       return `${state.loggedinUser.firstName} ${state.loggedinUser.lastName}`
     }
   },
+
   mutations: {
     signup(state, data) {
       state.singleUserForm.firstName = data.firstName;
@@ -68,6 +56,10 @@ export default createStore({
       state.singleUserForm.email = data.email;
       state.singleUserForm.password = data.password;
       state.singleUserForm.profilePicUrl = data.profilePicUrl;
+    },
+    deleteAccount(state) {
+      state.auth = {};
+      state.loggedinUser = {};
     },
     login(state, data) {
       state.auth.userId = data.userId;
@@ -81,38 +73,27 @@ export default createStore({
       state.auth = {};
       state.loggedinUser = {};
     },
-    deleteAccount(state) {
-      state.auth = {};
-      state.loggedinUser = {};
-    },
-    getAllUsers(state, data) {
-      state.allUsers = data;
-    },
+    
     getAllPosts(state, data) {
       state.allPosts = data;
     },
-    getSinglePost(state, data) { // singlePost
+    getSinglePost(state, data) {
       state.singlePost.postId = data.post_id;
       state.singlePost.title = data.title;
       state.singlePost.content = data.content;
       state.singlePost.contentImgUrl = data.contentImgUrl;
       state.singlePost.userId = data.userId;
-      state.singlePost.readPostId = data.readPostId;
-      state.singlePost.likePostId = data.likePostId;
-    },
-    getUnreadPosts(state, data) {
-      state.unreadPosts = data;
+      state.singlePost.ReadPosts = data.ReadPosts;
+      state.singlePost.LikePosts = data.LikePosts;
     },
     createSinglePost(state, data) {
       state.singlePostForm.title = data.title;
       state.singlePostForm.content = data.content;
       state.singlePostForm.contentImgUrl = data.contentImgUrl;
       state.singlePostForm.userId = data.userId;
-      // state.singlePostForm.readPostId = [],
-      // state.singlePostForm.likePostId = []
     },
     modifySinglePost(state, data) {
-      state.singlePostForm.postId = data.post_id; // is it okay to use singlePostForm that is the same with createSinglePost?
+      state.singlePostForm.postId = data.post_id;
       state.singlePostForm.title = data.title;
       state.singlePostForm.content = data.content;
       state.singlePostForm.contentImgUrl = data.contentImgUrl;
@@ -121,13 +102,20 @@ export default createStore({
     deleteSinglePost(state) {
       state.singlePost = {};
     },
-    readPost(state) {
-      state.singlePost.readPostId.push(state.auth.userId);
+    // readPost(state) {
+    //   state.singlePost.readPostId.push(state.auth.userId);
+    // },
+    // likePost(state) {
+    //   state.singlePost.likePostId.push(state.auth.userId);
+    // },
+    getUnreadPosts(state, data) {
+      state.unreadPosts = data;
     },
-    likePost(state) {
-      state.singlePost.likePostId.push(state.auth.userId);
-    }
+    getAllUsers(state, data) {
+      state.allUsers = data;
+    },
   },
+
   actions: {
     async signup({ commit }, { firstName, lastName, email, password, profilePicUrl }) {
       const user = {
@@ -138,7 +126,7 @@ export default createStore({
           password: password,
           profilePicUrl: profilePicUrl
         }
-      }
+      };
       try {
         const signupResult = await UserDataService.signup(user);
         if (signupResult.data) {
@@ -150,11 +138,24 @@ export default createStore({
         console.log(error);
       }
     },
+    async deleteAccount({ commit, state }) {
+      const userId = state.auth.userId;
+      try {
+        const deleteAccountResult = await UserDataService.deleteUser(userId);
+        if (deleteAccountResult.data) {
+          commit("deleteAccount");
+        } else {
+          console.log("Delete account failed!");
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    },
     async login({ commit }, { email, password }) {
       const user = {
         email: email,
         password: password
-      }
+      };
       try {
         const loginResult = await UserDataService.login(user);
         if (loginResult.data) {
@@ -178,15 +179,96 @@ export default createStore({
         console.log(error);
       }
     },
-    async deleteAccount({ commit, state }) {
-        const userId = state.auth.userId;
-        const deleteAccountResult = await UserDataService.deleteUser(userId);
-        console.log({deleteAccountResult})
-        if (deleteAccountResult.data) {
-          commit("deleteAccount");
-        } else {
-          console.log("Delete account failed!");
+    
+    async getAllPosts({ commit }) {
+      try {
+        const getAllPostsResult = await PostDataService.getAllPosts();
+        if (getAllPostsResult.data) {
+          commit("getAllPosts", getAllPostsResult.data);
         }
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    async getSinglePost({ commit }) {
+      const postId = parseInt(new URLSearchParams(window.location.href.split("/posts/")[1]));
+      try {
+        const getSinglePostResult = await PostDataService.getOnePost(postId);
+        if (getSinglePostResult.data) {
+          commit("getSinglePost", getSinglePostResult.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async createSinglePost({ commit, state }, { title, content, contentImgUrl }) {
+      const post = {
+        post: {
+          title: title,
+          content: content,
+          contentImgUrl: contentImgUrl,
+          userId: state.auth.userId
+        }
+      };
+      try {
+        const createSinglePostResult = await PostDataService.createPost(post);
+        if (createSinglePostResult.data) {
+          commit("createSinglePost", createSinglePostResult.data);
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    async modifySinglePost({ commit, state }, { postId, title, content, contentImgUrl }) {
+      const post = {
+        title: title,
+        content: content,
+        contentImgUrl: contentImgUrl,
+        userId: state.auth.userId
+      };
+      try {
+        const modifySinglePostResult = await PostDataService.modifyPost(postId, { post });
+        if (modifySinglePostResult.data) {
+          commit("modifySinglePost", modifySinglePostResult.data);
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    async deleteSinglePost({ commit, state }, { postId }) {
+      try {
+        const deleteSinglePostResult = await PostDataService.deletePost(postId);
+        if (deleteSinglePostResult.data) {
+          commit("deleteSinglePost");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async readPost({ commit }, { postId, userId }) {
+      try {
+        await PostDataService.readPost(postId, { userId });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async likePost({ commit }, { postId, userId }) {
+      try {
+        await PostDataService.likePost(postId, { userId });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getUnreadPosts({ commit, state }) {
+      const userId = state.auth.userId;
+      try {
+        const getUnreadPostsResult = await PostDataService.getUnreadPosts(userId);
+        if (getUnreadPostsResult.data) {
+          commit("getUnreadPosts", getUnreadPostsResult.data);
+        }
+      } catch(error) {
+        console.log(error)
+      }     
     },
     async getAllUsers({ commit }) {
       try {
@@ -198,114 +280,14 @@ export default createStore({
       } catch(error) {
         console.log(error);
       }
-
     },
-    async getAllPosts({ commit }) {
-      try {
-        const getAllPostsResult = await PostDataService.getAllPosts();
-        if (getAllPostsResult.data) {
-          commit("getAllPosts", getAllPostsResult.data);
-
-        }
-      } catch(error) {
-        console.log(error);
-      }
-    },
-    async getSinglePost({ commit }) {
-      const postId = new URLSearchParams(window.location.href.split("/posts/")[1]);
-      try {
-        const getSinglePostResult = await PostDataService.getOnePost(postId);
-        if (getSinglePostResult.data) {
-          commit("getSinglePost", getSinglePostResult.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async getUnreadPosts({commit}) {
-        const getUnreadPostsResult = await PostDataService.getUnreadPosts();
-        console.log({getUnreadPostsResult});
-        if (getUnreadPostsResult.data) {
-          commit("getUnreadPosts", getUnreadPostsResult.data);
-        }
-    },
-    async createSinglePost({ commit, state }, { title, content, contentImgUrl, userId, readPostId, likePostId }) {
-      const post = {
-        post: {
-          title: title,
-          content: content,
-          contentImgUrl: contentImgUrl,
-          userId: state.auth.userId
-        }
-      }
-      try {
-        const createSinglePostResult = await PostDataService.createPost(post);
-        if (createSinglePostResult.data) {
-          commit("createSinglePost", createSinglePostResult.data);
-        } else {
-          console.log("Post failed!");
-        }
-      } catch(error) {
-        console.log(error);
-      }
-    },
-    async modifySinglePost({ commit, state }, { title, content, contentImgUrl }) {
-      const postId = new URLSearchParams(window.location.href.split("/modify/")[1]);
-      const post = {
-        title: title,
-        content: content,
-        contentImgUrl: contentImgUrl,
-        userId: state.auth.userId
-      }
-      console.log(post)
-      try {
-        const modifySinglePostResult = await PostDataService.modifyPost(postId, { post });
-        console.log({modifySinglePostResult})
-        if (modifySinglePostResult.data) {
-          commit("modifySinglePost", modifySinglePostResult.data);
-        }
-      } catch(error) {
-        console.log(error);
-      }
-    },
-    async deleteSinglePost({ commit, state }) {
-      const postId = new URLSearchParams(window.location.href.split("/posts/")[1]);
-      console.log({postId})
-      try {
-        const deleteSinglePostResult = await PostDataService.deletePost(postId);
-        console.log({deleteSinglePostResult})
-        if (deleteSinglePostResult.data) {
-          commit("deleteSinglePost");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async readPost({ commit }, { postId, userId }) {
-      try {
-        const readPostResult = await PostDataService.readPost(postId, { userId });
-        console.log({readPostResult})
-        if (readPostResult.data) {
-          commit("readPost", readPostResult.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async likePost({ commit }, { postId, userId }) {
-      try {
-        // like === 1, not like === 0
-        const likePostResult = await PostDataService.likePost(postId, { userId });
-        console.log({likePostResult})
-        if (likePostResult.data) {
-          commit("likePost", likePostResult.data);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
   },
+
   modules: {
   },
-  plugins: [createPersistedState()]
-})
+
+  plugins: [
+    createPersistedState()
+  ]
+
+});
