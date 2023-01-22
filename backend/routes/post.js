c7de87fd9f65a9da8b1a8a01a8fbb7c8d70eee7d
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-22 13:10:07 +0900

    revise execution order of getUnreadPosts

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -7,12 +7,11 @@ const multer = require("../middleware/multer-config");
 const postCtrl = require("../controllers/post");
 
 router.get("/", auth, postCtrl.getAllPosts);
+router.get("/unread/:id", auth, postCtrl.getUnreadPosts);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.post("/", auth, multer, postCtrl.createPost);
 router.put("/:id", auth, multer, postCtrl.modifyPost);
 router.delete("/:id", auth, postCtrl.deletePost);
-
-router.get("/unread", auth, postCtrl.getUnreadPosts);
 router.post("/:id/read", auth, postCtrl.readPost);
 router.post("/:id/like", auth, postCtrl.likePost);
 
