517b27060cb4c3a168de76808dfe3bb4a1384e17
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-22 13:08:12 +0900

    add file preview and revise upload file -- pending

--- frontend/src/components/PostCreate.vue
+++ frontend/src/components/PostCreate.vue
@@ -1,29 +1,41 @@
 <template>
   <main class="post"> <!-- how to apply semantic tags in vue-->
     <h1>Create a post</h1>
-    <div class="create-form">
-      <div class="mb-3">
-        <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" v-model="title">
-      </div>
-      <div class="mb-3">
-        <label for="content" class="form-label">Content</label>
-        <textarea class="form-control" id="content" rows="8" v-model="content"></textarea>
-      </div>
-      <div class="mb-3">
-        <label for="contentImg" class="form-label">Upload files</label>
-        <input class="form-control" type="file" id="contentImg" @change="uploadContentImg">
-      </div>
+    <form method="post" enctype="multipart/form-data">
+      <div class="create-form">
+        <div class="mb-3">
+          <label for="title" class="form-label">Title</label>
+          <input type="text" class="form-control" id="title" v-model="title">
+        </div>
+        <div class="mb-3">
+          <label for="content" class="form-label">Content</label>
+          <textarea class="form-control" id="content" rows="8" v-model="content"></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="contentImgUrl" class="form-label">Upload file</label>
+          <input
+            class="form-control"
+            type="file"
+            accept="image/png, image/jpeg, image/jpg"
+            id="contentImgUrl"
+            name="contentImgUrl"
+            @change="uploadContetnImg"
+          >
+        </div>
+        <div class="mb-3" v-if="contentImgUrl">
+          <img :src="contentImgUrl" alt="image preview" width="250"/>
+        </div>
 
-      <p class="error" v-if="errors.length">
-        <b>Please fill up the following field(s):</b>
-        <ul>
-          <li v-for="error in errors" v-bind:key="error.index">{{ error }}</li>
-        </ul>
-      </p>
+        <p class="error" v-if="errors.length">
+          <b>Please fill up the following field(s):</b>
+          <ul>
+            <li v-for="error in errors" v-bind:key="error.index">{{ error }}</li>
+          </ul>
+        </p>
 
-      <button class="form-btn" type="button" @click="createPost">Post</button>
-    </div>
+        <button class="form-btn" type="button" @click="createPost">Post</button>
+      </div>
+    </form>
   </main>
 </template>
 
@@ -43,17 +55,35 @@ export default {
     }
   },
   methods: {
-    uploadContentImg(event) {
+    uploadContetnImg(event) {
       const file = event.target.files[0]
       this.createContentImg(file)
+      console.log(file)
+      console.log("this.contentImgUrl", this.contentImgUrl)
     },
-    createContentImg(file){
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
-      reader.onload = () => {
-        this.contentImg = reader.result
+    createContentImg(file) {
+      const reader = new FileReader();
+      reader.onload = event => {
+        console.log(event.target)
+        this.contentImgUrl = event.target.result;
+        // const encodedFile = dataUrl.replace("data:*/*;base64,", "");
+        // this.contentImgUrl = encodedFile;
+        // console.log("encodedfile: ", encodedFile)
       }
+      reader.readAsDataURL(file);
     },
+    // uploadContentImgUrl(event) {
+    //   const file = event.target.files[0]
+    //   this.createContentImgUrl(file)
+    // },
+    // createContentImgUrl(file){
+    //   const reader = new FileReader()
+    //   reader.readAsDataURL(file)
+    //   reader.onload = () => {
+    //     this.contentImgUrl = reader.result
+    //     console.log("createContentImgUrl", this.contentImgUrl)
+    //   }
+    // },
     checkUserForm() {
       this.errors = []
 
