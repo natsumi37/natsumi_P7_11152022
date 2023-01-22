11cd51c8a7aca63e9c78e02440143b7a4b73f5e5
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-22 12:56:16 +0900

    add no-profile-image file

--- frontend/src/components/EmployeesAll.vue
+++ frontend/src/components/EmployeesAll.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="post">
+  <main class="post">
     <h1>Our employees</h1>
     <div class="display-employees">
-      <div class="card" style="width: calc(25% - 20px);"  v-for="user in users" v-bind:key="user.user_id">
-        <img  class="card-img-top" :src="user.profilePicUrl" :alt="'profile picture of ' + user.firstName + ' ' + user.lastName">
+      <div class="card" v-for="user in users" v-bind:key="user.user_id"> <!-- style="width: calc(25% - 20px);"-->
+        <img class="card-img-top" v-if="user.profilePicUrl" :src="user.profilePicUrl" :alt="'profile picture of ' + user.firstName + ' ' + user.lastName">
+        <img class="card-img-top" v-else src="../../public/profile-no-image.png" :alt="'no image of ' + user.firstName + ' ' + user.lastName">
         <div class="card-body">
           <h5 class="card-title">{{ user.firstName }} {{ user.lastName }}</h5>
           <p class="card-text">{{ user.email }}</p>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -25,7 +26,6 @@ export default {
   computed: {
     ...mapState({
       users: "allUsers",
-
     })
   },
   beforeMount(){
@@ -40,7 +40,7 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .post {
   width: 100%;
   padding: 40px;
@@ -49,8 +49,23 @@ export default {
 .display-employees {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: flex-start;
   gap: 20px;
+  @media screen and (max-width: 450px) {
+    justify-content: center;
+  }
+}
+
+.card {
+  width: calc(33% - 20px);
+  object-fit: cover;
+  align-items: stretch;
+  @media screen and (max-width: 895px) {
+    width: calc(50% - 20px);
+  }
+  @media screen and (max-width: 450px) {
+    width: 90%;
+  }
 }
 
 </style>
--- frontend/src/components/HomeHeader.vue
+++ frontend/src/components/HomeHeader.vue
@@ -1,7 +1,8 @@
 <template>
   <header class="header-box">
     <img class="header-logo" src="@/assets/logo/icon-left-font-monochrome-white.png" alt="Groupomania logo">
-    <div class="header-profile-pic" :src="loggedinUser.profilePicUrl" :alt="'profile picture of ' + userFullName"></div>
+    <img class="header-profile-pic" v-if="loggedinUser.profilePicUrl" :src="loggedinUser.profilePicUrl" :alt="'profile picture of ' + loggedinUser.firstName + ' ' + loggedinUser.lastName">
+    <img class="header-profile-pic" v-else src="../../public/profile-no-image-icon.png" :alt="'no profile image of ' + loggedinUser.firstName + ' ' + loggedinUser.lastName">
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -29,7 +30,6 @@ export default {
       "auth", "loggedinUser"
     ]),
     ...mapGetters({
-      userFullName: "getUserFullName"
     }),
   },
   methods: {
@@ -87,7 +87,7 @@ export default {
   &-profile-pic {
     width: 50px;
     height: 50px;
-    border: white solid 1px;
+    // border: white solid 1px;
     border-radius: 50%;
     margin-left: auto;
     // object-fit: cover;
