43b24efcd9c1d7a72bb12e308e87d99cf12ce1c7
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-28 01:59:21 +0900

    delete unnecessary code

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Groupomania!" })
 });
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", userRoutes);
 app.use("/posts", postRoutes);
