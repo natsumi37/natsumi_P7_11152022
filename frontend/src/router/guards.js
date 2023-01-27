db14612f606b3e097c33ee59cda7d8d360abf9ee
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-28 02:12:45 +0900

    correct errors

--- frontend/src/router/guards.js
+++ frontend/src/router/guards.js
@@ -1,8 +1,13 @@
 import store from "@/store/index.js";
 
 export const authorizedToken = (to, from, next) => {
-  if (to.matched.some(route => route.meta.requiresAuth) && store.state.auth.token === "") {
-    next("/login");
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    if (!store.state.auth.token) {
+      next("/login");
+    }
+    else {
+      next();
+    }
   } else {
     next();
   }
