7a2906e44754f2de2cb263db0b22ee9d6876d26b
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2023-01-28 02:04:45 +0900

    delete unnecessary code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,9 +10,5 @@ router.post("/signup", multer, userCtrl.signup);
 router.post("/login", userCtrl.login);
 router.delete("/delete/:id", auth, userCtrl.delete);
 router.get("/users", auth, userCtrl.getAllUsers);
-/* how can I set the url of deleting an account 
- /api/auth/userId?
- /api/posts/userId?
-*/
 
 module.exports = router;
